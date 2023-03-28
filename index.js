ee439a64436cc42b3873080dcfc010caa68bddc3
VinayGupta <91736791+WinayGupta777@users.noreply.github.com>
2023-03-28 09:58:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("/", (req, res)=>{
     res.send(`
     <div style="text-align:center;">
-    <h1 style="font-size: 250%"> Welcome to Demo App!</h1>
+    <h1 style="font-size: 250%"> Welcome to Demo App [Try2]!</h1>
     <p style="font-size: 250%"> ${ip.address()} </p>
     </div>
     `);
